0e1c9a6ac8600cba4a3047b1e369a351f0ecad0e
roni <cicerone_mihalache@yahoo.com>
2009-12-19 12:00:38 -0500

    Replaced the "hard_coded" dirs with better names.

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+
 export PYTHON_INCLUDE=/usr/include/python2.5
+export SYSTEMC_INCLUDE=/usr/local/systemc-2.2.0/include
+export SCV_INCLUDE=/usr/local/scv-1.0p2-sysc2.2/include
+export TLM_INCLUDE=/usr/local/TLM-2009-07-15/include/tlm
+
+export SYSTEMC_LIB=/usr/local/systemc-2.2.0/lib-linux
+export SCV_LIB=/usr/local/scv-1.0p2-sysc2.2/lib-linux
+
+
 make clean 
 cmake clean .
 cmake .
