4ff9486a56ffcd5a6ab56f695c28bab8775cde98
roni <cicerone_mihalache@yahoo.com>
2009-12-21 07:02:29 -0500

    Split the program options class in two, one common (base) and one specific to the project.

---
+++ src/program_options/program_options_base.cpp
@@ -0,0 +1,92 @@
+/*===============================================================================================
+    Copyright (c) 2009 Kotys LLC. Distributed under the Boost Software License, Version 1.0. 
+    (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+    Author: Cicerone Mihalache                                     
+    Support: kosym@kotys.biz 
+===============================================================================================*/
+
+#include "program_options_base.h"
+
+using namespace std;
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////
+ProgramOptionsBase::ProgramOptionsBase() : 
+   mp_generic_options    (0),
+   mp_config_options     (0),
+   mp_hidden_options     (0), 
+   mp_cmdline_options    (0),
+   mp_config_file_options(0),
+   mp_visible_options    (0),
+   mp_variable_map       (0),
+   mp_positional_options (0),
+   m_config_file         ("NOT_INITIALIZED!")
+
+{
+   mp_generic_options     = new po::options_description("Generic options");
+   mp_config_options      = new po::options_description("Configuration");
+   mp_hidden_options      = new po::options_description("Hidden options");
+   mp_cmdline_options     = new po::options_description("Command Line options");
+   mp_config_file_options = new po::options_description("Configuration File options");
+   mp_visible_options     = new po::options_description("Visible options");
+   mp_variable_map        = new po::variables_map;
+   mp_positional_options  = new po::positional_options_description;
+
+};
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////
+ProgramOptionsBase::~ProgramOptionsBase()  
+{
+    if (mp_generic_options     != 0 ) delete mp_generic_options  ;
+    if (mp_config_options      != 0 ) delete mp_config_options   ;
+    if (mp_hidden_options      != 0 ) delete mp_hidden_options   ;
+    if (mp_cmdline_options     != 0 ) delete mp_cmdline_options     ;
+    if (mp_config_file_options != 0 ) delete mp_config_file_options ;
+    if (mp_visible_options     != 0 ) delete mp_visible_options     ;
+    if (mp_variable_map        != 0 ) delete mp_variable_map      ;
+    if (mp_positional_options  != 0 ) delete mp_positional_options; 
+
+};
+
+/////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////
+void ProgramOptionsBase::RegisterConfiguration(int ac_, char* p_av_[])
+{
+    (*mp_cmdline_options).add(*mp_generic_options);
+    (*mp_config_file_options).add(*mp_config_options);
+    (*mp_visible_options).add(*mp_cmdline_options).add(*mp_config_options);
+
+//parse command line
+    store(po::command_line_parser(ac_, p_av_).options(*mp_cmdline_options).run(), *mp_variable_map);
+    notify(*mp_variable_map);
+
+//parse config file        
+    std::ifstream ifs(m_config_file.c_str());
+    store(po::parse_config_file(ifs, *mp_config_file_options), *mp_variable_map);
+    notify(*mp_variable_map);
+}
+/////////////////////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////////////////////
+void ProgramOptionsBase::Help()
+{
+    if (mp_variable_map->count("help")) {
+        cout << *mp_visible_options << "\n";
+        exit(0);;
+    }
+    
+    if (mp_variable_map->count("version")) {
+        cout << "Kosim version 0.1\n";
+        exit(0);;
+    }
+}
+
---
+++ src/program_options/program_options_base.h
@@ -0,0 +1,48 @@
+/*===============================================================================================
+    Copyright (c) 2009 Kotys LLC. Distributed under the Boost Software License, Version 1.0. 
+    (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+    Author: Cicerone Mihalache                                     
+    Support: kosym@kotys.biz 
+===============================================================================================*/
+
+#ifndef KOSIM_PO_PROGRAM_OPTIONS_BASE_H
+#define KOSIM_PO_PROGRAM_OPTIONS_BASE_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdint.h>
+
+#include <boost/utility.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+
+class ProgramOptionsBase : boost::noncopyable
+{
+public:
+    void Help();
+
+    ProgramOptionsBase();
+    ~ProgramOptionsBase();
+    virtual void InitConfiguration() {};
+    void   RegisterConfiguration(int ac_, char* p_av_[]);
+
+protected:
+
+    po::options_description* mp_generic_options    ;
+    po::options_description* mp_config_options     ;
+    po::options_description* mp_hidden_options     ;
+    po::options_description* mp_cmdline_options    ;
+    po::options_description* mp_config_file_options;
+    po::options_description* mp_visible_options    ;
+    po::variables_map*       mp_variable_map       ;
+    po::positional_options_description* mp_positional_options;
+    std::string m_config_file;    // configuration pattern file
+
+private:
+
+};
+
+#endif // KOSIM_PO_PROGRAM_OPTIONS_BASE_H
