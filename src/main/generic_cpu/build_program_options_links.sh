363079d2936f5e4c17c8e0d762235261aa1925f9
roni <cicerone_mihalache@yahoo.com>
2009-12-22 00:40:48 -0500

    Build the symbolic links for the program options when buiild.sh is run.

---
+++ src/main/generic_cpu/build_program_options_links.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -s ~/github_proj/kosim/src/program_options/options_builder.h         ~/github_proj/kosim/src/main/generic_cpu/options_builder.h
+ln -s ~/github_proj/kosim/src/program_options/options_builder.cpp       ~/github_proj/kosim/src/main/generic_cpu/options_builder.cpp
+ln -s ~/github_proj/kosim/src/program_options/program_options_base.h    ~/github_proj/kosim/src/main/generic_cpu/program_options_base.h
+ln -s ~/github_proj/kosim/src/program_options/program_options_base.cpp  ~/github_proj/kosim/src/main/generic_cpu/program_options_base.cpp
+
