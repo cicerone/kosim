941046b7bbd477e81a5ae5fe555b238a550e35a6
roni <cicerone_mihalache@yahoo.com>
2010-01-11 08:03:27 -0500

    Added read/write methods.

--- src/memory_map/memory_map.cpp
+++ src/memory_map/memory_map.cpp
@@ -11,17 +11,62 @@ using namespace std;
 
 /////////////////////////////////////////////////////////////////////////////////////
 //
-////////////////////////////////////////////////////////////////////////////////////
+// IN: 
+// OUT: 
+// RET: 
 MemoryMap::MemoryMap(const uint32_t id_, const string& name_, const uint32_t offset_) :
-  m_id   (id_     ),
-  m_name (name_   ),
-  m_offset(offset_)
+  m_id              (id_    ),
+  m_name            (name_  ),
+  m_offset          (offset_),
+  m_number_registers(0      ),
+  m_memory_size     (0      )
 {
+}
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// IN: 
+// OUT: 
+// RET: 
+MemoryMap::~MemoryMap()  
+{
+}
+/////////////////////////////////////////////////////////////////////////////////////
+// allocates the memory needed for registers and the memory bank
+// IN:  num_regs_ - the number of registers
+//      mem_size_ - the size of the memory bank
+// OUT: 
+// RET: 
+void MemoryMap::SetSpaceSize(const uint32_t num_regs_, const uint32_t mem_size_)
+{
+    m_number_registers = num_regs_;
+    m_memory_size      = mem_size_;
+    uint32_t size = num_regs_ + mem_size_;
+    m_hw_resource.resize(size, 0);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
 //
-////////////////////////////////////////////////////////////////////////////////////
-MemoryMap::~MemoryMap()  
+// IN:   addr_ - the address of reg/memory; it is 4 byte aligned 
+//       data_ - the value to be written
+// OUT: 
+// RET:  true if the operation was succesfull
+bool MemoryMap::write(const uint32_t addr_, const uint32_t data_)
 {
+    uint32_t local_addr = addr_ >> 2;
+    if (local_addr > m_hw_resource.size()) return false; // out of space
+    m_hw_resource[local_addr] = data_;
+    return true;
 }
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// IN: 
+// OUT: 
+// RET: 
+bool MemoryMap::read (const uint32_t addr_, uint32_t* const p_data_)
+{
+    uint32_t local_addr = addr_ >> 2;
+    if (local_addr > m_hw_resource.size()) return false; // out of space
+    *p_data_ = m_hw_resource[local_addr];
+    return true;
+}
+
--- src/memory_map/memory_map.h
+++ src/memory_map/memory_map.h
@@ -8,9 +8,11 @@
 #ifndef KOSIM_MM_MEMORY_MAP_H
 #define KOSIM_MM_MEMORY_MAP_H
 
-#include <stdint.h>
 #include <string>
+#include <vector>
+#include <stdint.h>
 
+#include "systemc.h"
 #include <boost/utility.hpp>
 
 using namespace std;
@@ -20,10 +22,22 @@ class MemoryMap : boost::noncopyable
 public:
     MemoryMap(const uint32_t id_, const string& name_, const uint32_t offset_);
     ~MemoryMap();
+    uint32_t get_id()     { return m_id;};
+    string*  get_name()   { return &m_name;};
+    uint32_t get_offset() { return m_offset;};
+    void     SetSpaceSize(const uint32_t num_regs_, const uint32_t mem_size_);
+    uint32_t get_number_regs() { return m_number_registers;};
+    uint32_t get_memory_size() { return m_memory_size;};
+    bool     write(const uint32_t addr_, const uint32_t data_);
+    bool     read (const uint32_t addr_, uint32_t* const p_data_);    
+    
 private:
     uint32_t    m_id    ; 
     string      m_name  ; 
-    uint32_t    m_offset;
+    uint32_t    m_offset;           // 4bytes aligned
+    uint32_t    m_number_registers; // 4 bytes regs
+    uint32_t    m_memory_size;      // 4 bytes per memory location
+    vector<sc_uint<32> > m_hw_resource;
 };
 
 #endif // KOSIM_MM_MEMORY_MAP_H
