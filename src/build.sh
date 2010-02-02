e0d082ba07fb827f732f54d38d1c30d30dfa46a6
roni <cicerone_mihalache@yahoo.com>
2010-02-02 00:25:05 -0500

    Treat Boost libs like the other libs.

--- src/build.sh
+++ src/build.sh
@@ -6,25 +6,30 @@ INSTALL_PREFIX="/usr/local"
 SYSTEMC_ROOT="$INSTALL_PREFIX/systemc-2.2.0"
 SCV_ROOT="$INSTALL_PREFIX/scv-1.0p2-sysc2.2"
 TLM_ROOT="$INSTALL_PREFIX/TLM-2009-07-15"
+BOOST_ROOT="INSTALL_PREFIX/boost_1_37_0"
 
 # Include paths:
 export PYTHON_INCLUDE="/usr/include/python`python --version 2>&1 | cut -d ' ' -f2 | cut -d '.' -f1-2`"
 export SYSTEMC_INCLUDE="$SYSTEMC_ROOT/include"
 export SCV_INCLUDE="$SCV_ROOT/include"
 export TLM_INCLUDE="$TLM_ROOT/include/tlm"
+export BOOST_INCLUDE="$BOOST_ROOT/include/boost-1_37"
 
 # Library paths:
 LIB_DIR="lib-linux"
 if test "`uname -m`" == "x86_64"; then LIB_DIR="lib-linux64"; fi
 export SYSTEMC_LIB="$SYSTEMC_ROOT/$LIB_DIR"
 export SCV_LIB="$SCV_ROOT/$LIB_DIR"
+export BOOST_LIB="$BOOST_ROOT/lib"
 
 echo "PYTHON_INCLUDE  = $PYTHON_INCLUDE "
 echo "SYSTEMC_INCLUDE = $SYSTEMC_INCLUDE"
 echo "SCV_INCLUDE     = $SCV_INCLUDE    "
 echo "TLM_INCLUDE     = $TLM_INCLUDE    "
+echo "BOOST_INCLUDE   = $BOOST_INCLUDE  "
 echo "SYSTEMC_LIB     = $SYSTEMC_LIB    "
 echo "SCV_LIB         = $SCV_LIB        "
+echo "BOOST_LIB       = $BOOST_LIB      "
 
 python main/generic_cpu/test1/build_program_options_links.py
 python main/generic_cpu/test2/build_program_options_links.py
